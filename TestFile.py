8deea773be9570f0dd72d7ba89117b0c4bca92dd
aiiliev <alexiliev85@gmail.com>
2023-11-30 14:21:14 +0100

    Save a py file for adding two numbers. Update of the py file.

--- TestFile.py
+++ TestFile.py
@@ -3,4 +3,11 @@ def sum_of_two ():
     number2 = input("Please enter number2: ")
     return print("The sum of the two numbers is:", int(number1) + int(number2))
 
-sum_of_two()
+# sum_of_two()
+
+while number != '0':
+    number = input("Please give a number: ")
+    empty_list.append(int(number))
+    print(empty_list)
+
+# avg_of_list(empty_list)
